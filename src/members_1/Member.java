2c65f3ab53d3ba2e194c7c700ebfb1402d3eb0b0
test123 <a@a.com>
2024-02-15 17:07:02 +0900

    members

---
+++ src/members_1/Member.java
@@ -0,0 +1,12 @@
+package members_1;
+
+public class Member {
+// public static void main(String[] args) {
+//	String name, addr, age;
+//	name = "홍길동"; addr = "산골짜기"; age = "20";
+	public String name, addr;
+	public int age;
+
+
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+//프로젝트를 내보내거나 가지고올때 사용
+module members {
+	
+	exports members_1; //내보내겠다.
+	
+}
